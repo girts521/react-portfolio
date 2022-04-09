6cdc67967d79e445713d35045f34c87b4ca090cd
girts521 <girts521@gmail.com>
2022-04-09 09:01:08 +0200

    updated api link

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -26,7 +26,7 @@ const Contact: React.FC = () => {
       emailRef.current!.value = "";
       messageRef.current!.value = "";
 
-      const response = await fetch("http://localhost:3001/send", {
+      const response = await fetch("https://gkarcevskis-portfolio-api.herokuapp.com/send", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
